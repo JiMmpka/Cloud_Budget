bddf8f2449ae4b2bed6bfae947ef6de558ebe25f
PScode <jimmpka.github@gmail.com>
2025-02-20 13:15:48 +0100

    update

---
+++ src/App/Services/ReceiptService.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use Framework\Database;
+use Framework\Exceptions\ValidationException;
+
+class ReceiptService
+{
+    public function __construct(private Database $db) {}
+
+    public function validateFiles(?array $file)
+    {
+        if (!$file || $file['error'] !== UPLOAD_ERR_OK) {
+            throw new ValidationException([
+                'receipt' => ['Failed to upload file']
+            ]);
+        }
+
+        $maxFileSizeMB = 3 * 1024 * 1024;
+
+        if ($file['size'] > $maxFileSizeMB) {
+            throw new ValidationException([
+                'receipt' => ['File upload is too large']
+            ]);
+        }
+
+        $originalFileName = $file['name'];
+
+        if (!preg_match('/^[A-za-z0-9\s._-]+$/', $originalFileName)) {
+            throw new ValidationException([
+                'receipt' => ['Invalid filename']
+            ]);
+        }
+
+        dd($file);
+    }
+}
