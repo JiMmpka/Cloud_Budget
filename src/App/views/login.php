2cb0a51968779f8911e133f2ca4f840c392d8200
PScode <jimmpka.github@gmail.com>
2025-05-13 08:10:50 +0200

    feature: add backend to login page to handle email and password errors

--- src/App/views/login.php
+++ src/App/views/login.php
@@ -1,31 +1,110 @@
-<?php include $this->resolve("partials/_header.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<section class="max-w-2xl mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded">
-    <form method="POST" class="grid grid-cols-1 gap-6">
-        <?php include $this->resolve('partials/_csrf.php'); ?>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Claud Budget</title>
+  <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/components/logins/login-9/assets/css/login-9.css">
+  <link rel="stylesheet" href="/assets/css/auth.css">
+  <link rel="stylesheet" href="/assets/css/main.css">
+</head>
 
-        <label class="block">
-            <span class="text-gray-700">Email address</span>
-            <input value="<?php echo e($oldFormData['email'] ?? ''); ?>" name="email" type="email" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="john@example.com" />
-            <?php if (array_key_exists('email', $errors)) : ?>
-                <div class="bg-gray-100 mt-2 p-2 text-red-500">
-                    <?php echo e($errors['email'][0]); ?>
+<body>
+
+  <!-- Login Component -->
+  <section class="bg-primary d-flex align-items-center min-vh-100 py-4">
+    <div class="container">
+      <div class="row gy-4 justify-content-center align-items-center">
+        <div class="col-12 col-md-6 col-xl-7">
+          <div class="d-flex justify-content-center text-bg-primary">
+            <div class="col-12 col-xl-9">
+              <img class="mb-4 img-fluid" loading="lazy" src="/assets/images/BC-logo.png" alt="Cloud Budget Logo">
+              <h2 class="h1 mb-4">Make your budget fly</h2>
+              <p class="lead fw-normal bm-5">With Claud Budget you will take your budget to another level of management. Add incomes, expenses, check balance. Take control of your home budget lightly and comfortable.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-xl-5">
+          <div class="card border-0 rounded-4">
+            <div class="card-body p-3 p-md-4 p-xl-5">
+              <div class="row">
+                <div class="col-12">
+                  <div class="mb-4">
+                    <h3>Sign in</h3>
+                    <p>Don't have an account? <a href="/register">Sign up</a></p>
+                  </div>
                 </div>
-            <?php endif; ?>
-        </label>
-        <label class="block">
-            <span class="text-gray-700">Password</span>
-            <input name="password" type="password" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="" />
-            <?php if (array_key_exists('password', $errors)) : ?>
-                <div class="bg-gray-100 mt-2 p-2 text-red-500">
-                    <?php echo e($errors['password'][0]); ?>
-                </div>
-            <?php endif; ?>
-        </label>
-        <button type="submit" class="block w-full py-2 bg-indigo-600 text-white rounded">
-            Submit
-        </button>
-    </form>
-</section>
+              </div>
 
-<?php include $this->resolve("partials/_footer.php"); ?>
+              <form method="POST">
+                <?php include $this->resolve('partials/_csrf.php'); ?>
+                <div class="row gy-3 overflow-hidden">
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input
+                        value="<?php echo e($oldFormData['email'] ?? ''); ?>"
+                        name="email"
+                        type="email"
+                        class="form-control <?php echo array_key_exists('email', $errors) ? 'is-invalid' : ''; ?>"
+                        placeholder="name@example.com"
+                        required>
+                      <label for="email" class="form-label">Email</label>
+                      <?php if (array_key_exists('email', $errors)) : ?>
+                        <div class="invalid-feedback">
+                          <?php echo e($errors['email'][0]); ?>
+                        </div>
+                      <?php endif; ?>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <div class="form-floating mb-3">
+                      <input
+                        type="password"
+                        class="form-control <?php echo array_key_exists('password', $errors) ? 'is-invalid' : ''; ?>"
+                        name="password"
+                        id="password"
+                        placeholder="Password"
+                        required>
+                      <label for="password" class="form-label">Password</label>
+                      <?php if (array_key_exists('password', $errors)) : ?>
+                        <div class="invalid-feedback">
+                          <?php echo e($errors['password'][0]); ?>
+                        </div>
+                      <?php endif; ?>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" value="" name="remember_me" id="remember_me">
+                      <label class="form-check-label text-secondary" for="remember_me">
+                        Keep me logged in
+                      </label>
+                    </div>
+                  </div>
+                  <div class="col-12">
+                    <div class="d-grid">
+                      <button class="btn btn-primary btn-lg" type="submit">Log in now</button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+
+              <div class="row">
+                <div class="col-12">
+                  <div class="d-flex gap-2 gap-md-4 flex-column flex-md-row justify-content-md-end mt-4">
+                    <a href="#!">Forgot password</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
